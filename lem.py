0eea34e0cbc16c5bf2ae7bb94bb5a0f4dc1e96a6
mh <mh@libavg.de>
2009-05-12 21:11:18 +0000

    avg_media/mtc/lemming: added license
    svn path=/trunk/avg_media/mtc/lemming/; revision=3855

--- lem.py
+++ lem.py
@@ -1,3 +1,20 @@
+#!/usr/bin/env python
+# Copyright (C) 2009
+#    Martin Heistermann, <mh at sponc dot de>
+#
+# lem.py is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# lem.py is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with lem.py.  If not, see <http://www.gnu.org/licenses/>.
+
 import os
 import ConfigParser
 from libavg import AVGApp, avg, Point2D
